de9029733d23297b46fbc4e9526e090eca8b3121
Shinji Ikari <mishranirmit27@gmail.com>
2024-09-29 17:45:08 +0530

    Part 3 Exercises 3.1 to 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,34 +1,97 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-
-let notes = [
+let persons = [
   {
     id: "1",
-    content: "HTML is easy.",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: "2",
-    content: "Browser can execute only JavaScript.",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol.",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello friend." });
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.get("/info", (req, res) => {
+  const currentDate = new Date().toString("en-US");
+  res.status(200).render("index", {
+    title: "Phonebook",
+    count: persons.length,
+    currentDate: currentDate,
+  });
 });
 
-app.get("/api/notes", (req, res) => {
-  res.status(200).json({ data: notes });
+app.get("/api/persons", (req, res) => {
+  res.status(200).json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const note = persons.filter((note) => note.id === id);
+
+  if (note.length !== 0) return res.status(200).json(note[0]);
+  else
+    res.status(404).json({ error: `Person with the id ${id} does not exist.` });
+});
+
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Person is missing a name or a number.",
+    });
+  }
+
+  const duplicate = persons.filter((person) => person.name === body.name);
+  if (duplicate.length !== 0) {
+    return res.status(400).json({
+      error: `${body.name} already exists in the phonebook. Name must be unique.`,
+    });
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1e6),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  res.status(201).json({ message: `${newPerson.name} added.` });
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const tbd = persons.filter((note) => note.id === id);
+
+  if (tbd.length === 0)
+    return res
+      .status(404)
+      .json({ error: `Person with the id ${id} does not exist.` });
+
+  persons = persons.filter((note) => note.id !== id);
+  res.status(204);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is live on port ${PORT}`);
+  console.log(`Server is live on port ${PORT} 🌐`);
 });
