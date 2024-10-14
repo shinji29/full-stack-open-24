afc27cf504ba413aaf7069570877d2c6290a4411
Shinji Ikari <mishranirmit27@gmail.com>
2024-10-14 20:40:02 +0530

    Delete part3/test directory
    Removing the UI directory since static build is being rendered from the backend itself.

--- part3/test/src/components/Message.jsx
+++
@@ -1,6 +0,0 @@
-function Message({ loading, error }) {
-  if (loading) return <p className="message">Loading...</p>;
-  if (error) return <p className="error">Error: {error}</p>;
-}
-
-export default Message;
